1d72781919bf88cb49143ad1c57c775890100eaa
Vimal Bharti <s.vimalbharti@gmail.com>
2024-02-20 22:33:38 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,11 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        rollupOptions: {
+          external: [
+            "toastify-js/src/toastify.css"
+          ]
+        }
+      }
 });
