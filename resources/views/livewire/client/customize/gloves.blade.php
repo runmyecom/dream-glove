de55744021d21fafc7b1ee142018edf34a2efaa7
Vimal Bharti <s.vimalbharti@gmail.com>
2024-04-14 20:16:39 +0530

    responsive added

--- resources/views/livewire/client/customize/gloves.blade.php
+++ resources/views/livewire/client/customize/gloves.blade.php
@@ -1,59 +1,58 @@
 <div class="bg-white w-full h-full">
-    <div class="flex flex-col md:flex-row">
+    <div class="grid md:grid-cols-2">
       <div class="canvas-wraper w-full md:h-[83vh] h-[40vh] flex items-center justify-center overflow-hidden">
-        <figure class="relative">
-            {{-- <img src="/builder/dream-gloves-1.png" class="md:h-[85vh] h-[40vh] object-contain"> --}}
-            <div class="relative">
-                <div class="carousel max-w-xl flex">
-                    <!-- Carousel items -->
-                    <div class="carousel-item relative">
-                        <img src="/builder/dream-gloves/Style-1-a.webp" alt="Carousel Image 1" class="md:h-[85vh] h-[40vh] object-contain w-full">
-                        <x-builders.dreamglove.bindingone class="absolute bottom-[6.6%] left-[24.4%] h-[40%] w-[44.7%]" :color="$bindingcolor" />
-                        <x-builders.dreamglove.wristtwo class="absolute bottom-[11.5%] right-[46.4%] w-[21%]" :color="$wristcolor" />
-                        <x-builders.dreamglove.homeplate class="absolute bottom-[27.5%] right-[45.3%] w-[10.6%]" :color="$homeplatecolor" />
-                        <x-builders.dreamglove.logoright class="absolute bottom-[28.9%] right-[47.9%] h-[7.8%]" :color="$logocolor" />
-                        <x-builders.dreamglove.logoleft class="absolute bottom-[14%] left-[32.8%] h-[9%]" :color="$logocolor" />
+        <figure class="relative h-full">
+            <div class="carousel h-full">
+                <!-- Carousel items -->
+                <div class="carousel-item relative h-full">
+                    <img src="/builder/dream-gloves/Style-1-a.webp" alt="Carousel Image 1" class="w-full h-full object-contain">
 
-                        <span class="font-bold absolute top-[40%] right-[31%] -rotate-[64deg] w-[120px] h-6 overflow-clip" style="color: {{$customTextColor}}">{{$customText}}</span>
+                    <x-builders.dreamglove.bindingone class="absolute bottom-[10.2%] left-[13%] w-[65%]" :color="$bindingcolor" />
+                    <x-builders.dreamglove.wristtwo class="absolute bottom-[11.5%] right-[43.7%] w-[31.5%]" :color="$wristcolor" />
+                    <x-builders.dreamglove.homeplate class="absolute bottom-[27.6%] left-[42%] w-[15%]" :color="$homeplatecolor" />
+                    <x-builders.dreamglove.logoright class="absolute bottom-[29%] right-[47.3%] w-[9.5%] z-10" :color="$logocolor" />
+                    <x-builders.dreamglove.logoleft class="absolute bottom-[14%] left-[25%] w-[9.6%] z-10" :color="$logocolor" />
 
-                        @if ($custombrand)
-                            <img src="{{ $custombrand->temporaryUrl() }}" class="absolute -rotate-[68deg] bottom-[26.5%] right-[45%] w-[62px] object-contain">
-                        @endif
-                    </div>
-                    <div class="carousel-item relative">
-                        <img src="/builder/dream-gloves/Style-1-b.webp" alt="Carousel Image 2" class="md:h-[85vh] h-[40vh] object-contain w-full">
-                        <x-builders.dreamglove.bindingtwo class="absolute bottom-[3.6%] left-[33.5%] h-[20%] w-[23%]" :color="$bindingcolor" />
-                        <x-builders.dreamglove.bindingthree class="absolute bottom-[14%] left-[14.8%] h-[20.5%] w-[60%]" :color="$bindingcolor" />
-                        <x-builders.dreamglove.fingerpad class="absolute bottom-[34.2%] right-[40.7%] w-[13%]" :color="$fingerpadcolor" />
-                        <x-builders.dreamglove.text class="absolute top-[24%] right-[40.4%] h-[22%]" :color="$textcolor" />
-                        <x-builders.dreamglove.wristone class="absolute bottom-[12%] left-[30%] h-[16%]" :color="$wristcolor" />
-                        <x-builders.dreamglove.logotop class="absolute bottom-[13.3%] left-[41.9%] h-[10%]" :color="$logocolor" />
-                    </div>
-                    <div class="carousel-item relative">
-                        <img src="/builder/dream-gloves/Style-1-c.webp" alt="Carousel Image 3" class="md:h-[85vh] h-[40vh] object-contain w-full">
-                    </div>
+                    <div class="font-bold absolute top-[40.5%] right-[22.8%] -rotate-[64deg] h-[4%] text-[1vw] overflow-clip w-[30%] flex items-center justify-center" style="color: {{$customTextColor}}">{{$customText}}</div>
+
+                    @if ($custombrand)
+                    <img src="{{ $custombrand->temporaryUrl() }}" class="absolute -rotate-[68deg] bottom-[25.5%] right-[40.6%] w-[80px] object-contain">
+                    @endif
                 </div>
+                <div class="carousel-item relative h-full">
+                    <img src="/builder/dream-gloves/Style-1-b.webp" alt="Carousel Image 2" class="w-full h-full object-contain">
 
-                <!-- Carousel controls -->
-                <div class="absolute inset-y-0 left-0 flex items-center justify-start pl-4">
-                    <button
-                        class="carousel-control-prev bg-gray-800 hover:bg-gray-700 text-white rounded-full p-2 focus:outline-none">
-                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
-                        </svg>
-                    </button>
+                    <x-builders.dreamglove.bindingtwo class="absolute bottom-[11%] left-[24.8%] w-[35%]" :color="$bindingcolor" />
+                    <x-builders.dreamglove.bindingthree class="absolute bottom-[14.3%] left-[17.8%] w-[48.4%]" :color="$bindingcolor" />
+                    <x-builders.dreamglove.fingerpad class="absolute bottom-[34.2%] right-[36.5%] w-[18.8%]" :color="$fingerpadcolor" />
+                    <x-builders.dreamglove.text class="absolute top-[24%] right-[35.9%] h-[22%]" :color="$textcolor" />
+                    <x-builders.dreamglove.wristone class="absolute bottom-[12.3%] left-[21.4%] w-[43.5%]" :color="$wristcolor" />
+                    <x-builders.dreamglove.logotop class="absolute bottom-[13.3%] left-[38.2%] h-[10%] z-10" :color="$logocolor" />
                 </div>
-                <div class="absolute inset-y-0 right-0 flex items-center justify-end pr-4">
-                    <button
-                        class="carousel-control-next bg-gray-800 hover:bg-gray-700 text-white rounded-full p-2 focus:outline-none">
-                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
-                        </svg>
-                    </button>
+                <div class="carousel-item relative">
+                    <img src="/builder/dream-gloves/Style-1-c.webp" alt="Carousel Image 3" class="md:h-[85vh] h-[40vh] object-contain w-full">
                 </div>
             </div>
+
+            <!-- Carousel controls -->
+            {{-- <div class="absolute inset-y-0 -left-16 flex items-center justify-start pl-4">
+                <button
+                    class="carousel-control-prev bg-gray-800/50 hover:bg-gray-700 text-white rounded-full p-2 focus:outline-none">
+                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
+                    </svg>
+                </button>
+            </div>
+            <div class="absolute inset-y-0 -right-16 flex items-center justify-end pr-4">
+                <button
+                    class="carousel-control-next bg-gray-800/50 hover:bg-gray-700 text-white rounded-full p-2 focus:outline-none">
+                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
+                    </svg>
+                </button>
+            </div> --}}
         </figure>
       </div>
       <!-- toolbar -->
