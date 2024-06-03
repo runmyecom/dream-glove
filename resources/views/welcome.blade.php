3b2f55b923d2792f077e026c7f18ffc717e244ff
Vimal Bharti <s.vimalbharti@gmail.com>
2024-06-03 09:46:13 +0530

    landing changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,8 +8,8 @@
             <p class="text-center mb-12">Experience sophistication with every piece - where elegance meets functionality in a <br/> perfect blend of style and practicality.</p>
             <div class="grid md:grid-cols-4 gap-5">
                 <div class="card">
-                    <div class="image bg-gray-100 p-5 rounded-xl overflow-hidden shadow md:h-[50vh] h-[30vh] flex items-center justify-center relative group cursor-pointer">
-                        <img src="/images/pro_1.png" alt="" class="group-hover:scale-125 transition duration-700 ease-in-out">
+                    <div class="image bg-gray-100 rounded-xl overflow-hidden shadow h-[48vh] flex items-center justify-center relative group cursor-pointer">
+                        <img src="/thumb/Dream-AS-Mint-Gold.jpeg" alt="" class="group-hover:scale-125 transition duration-700 ease-in-out w-full h-full object-cover">
                         <button class="bg-white shadow-xl rounded-full h-10 w-10 items-center justify-center absolute top-4 right-4 text-gray-700 hidden group-hover:flex transition duration-300 ease-in-out">
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24"><path fill="currentColor" d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5M12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5s5 2.24 5 5s-2.24 5-5 5m0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3s3-1.34 3-3s-1.34-3-3-3"/></svg>
                         </button>
@@ -17,12 +17,12 @@
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 256 256"><path fill="currentColor" d="M96 216a16 16 0 1 1-16-16a16 16 0 0 1 16 16m88-16a16 16 0 1 0 16 16a16 16 0 0 0-16-16m47.65-125.65l-28.53 92.71A23.89 23.89 0 0 1 180.18 184H84.07A24.11 24.11 0 0 1 61 166.59L24.82 40H8a8 8 0 0 1 0-16h16.82a16.08 16.08 0 0 1 15.39 11.6L48.32 64H224a8 8 0 0 1 7.65 10.35M213.17 80H52.89l23.49 82.2a8 8 0 0 0 7.69 5.8h96.11a8 8 0 0 0 7.65-5.65Z"/></svg>
                         </button>
                     </div>
-                    <h3 class="text-xl mt-2 font-bold">Dream As Baby Blue</h3>
+                    <h3 class="text-xl mt-2 font-bold">Dream AS Mint/Gold</h3>
                     <h4>$25.00 - $35.00</h4>
                 </div>
                 <div class="card">
-                    <div class="image bg-gray-100 p-5 rounded-xl overflow-hidden shadow md:h-[50vh] h-[30vh] flex items-center justify-center relative group cursor-pointer">
-                        <img src="/images/pro_2.png" alt="" class="group-hover:scale-125 transition duration-700 ease-in-out">
+                    <div class="image bg-gray-100 rounded-xl overflow-hidden shadow h-[48vh] flex items-center justify-center relative group cursor-pointer">
+                        <img src="/thumb/Dream-Belt-Black.png" alt="" class="group-hover:scale-125 transition duration-700 ease-in-out h-full w-full object-cover">
                         <button class="bg-white shadow-xl rounded-full h-10 w-10 items-center justify-center absolute top-4 right-4 text-gray-700 hidden group-hover:flex transition duration-300 ease-in-out">
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24"><path fill="currentColor" d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5M12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5s5 2.24 5 5s-2.24 5-5 5m0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3s3-1.34 3-3s-1.34-3-3-3"/></svg>
                         </button>
@@ -30,12 +30,12 @@
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 256 256"><path fill="currentColor" d="M96 216a16 16 0 1 1-16-16a16 16 0 0 1 16 16m88-16a16 16 0 1 0 16 16a16 16 0 0 0-16-16m47.65-125.65l-28.53 92.71A23.89 23.89 0 0 1 180.18 184H84.07A24.11 24.11 0 0 1 61 166.59L24.82 40H8a8 8 0 0 1 0-16h16.82a16.08 16.08 0 0 1 15.39 11.6L48.32 64H224a8 8 0 0 1 7.65 10.35M213.17 80H52.89l23.49 82.2a8 8 0 0 0 7.69 5.8h96.11a8 8 0 0 0 7.65-5.65Z"/></svg>
                         </button>
                     </div>
-                    <h3 class="text-xl mt-2 font-bold">Dream Classic Infield Glove 12</h3>
-                    <h4>$35.00</h4>
+                    <h3 class="text-xl mt-2 font-bold">Dream Belt Black</h3>
+                    <h4>$55.00</h4>
                 </div>
                 <div class="card">
-                    <div class="image bg-gray-100 p-5 rounded-xl overflow-hidden shadow md:h-[50vh] h-[30vh] flex items-center justify-center relative group cursor-pointer">
-                        <img src="/images/pro_3.png" alt="" class="group-hover:scale-125 transition duration-700 ease-in-out">
+                    <div class="image bg-gray-100 rounded-xl overflow-hidden shadow h-[48vh] flex items-center justify-center relative group cursor-pointer">
+                        <img src="/thumb/Dream-Sliding-Mitt-Smiley-Melt.png" alt="" class="group-hover:scale-125 transition duration-700 ease-in-out h-full w-full object-cover">
                         <button class="bg-white shadow-xl rounded-full h-10 w-10 items-center justify-center absolute top-4 right-4 text-gray-700 hidden group-hover:flex transition duration-300 ease-in-out">
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24"><path fill="currentColor" d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5M12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5s5 2.24 5 5s-2.24 5-5 5m0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3s3-1.34 3-3s-1.34-3-3-3"/></svg>
                         </button>
@@ -43,12 +43,12 @@
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 256 256"><path fill="currentColor" d="M96 216a16 16 0 1 1-16-16a16 16 0 0 1 16 16m88-16a16 16 0 1 0 16 16a16 16 0 0 0-16-16m47.65-125.65l-28.53 92.71A23.89 23.89 0 0 1 180.18 184H84.07A24.11 24.11 0 0 1 61 166.59L24.82 40H8a8 8 0 0 1 0-16h16.82a16.08 16.08 0 0 1 15.39 11.6L48.32 64H224a8 8 0 0 1 7.65 10.35M213.17 80H52.89l23.49 82.2a8 8 0 0 0 7.69 5.8h96.11a8 8 0 0 0 7.65-5.65Z"/></svg>
                         </button>
                     </div>
-                    <h3 class="text-xl mt-2 font-bold">Dream Elbow Guard</h3>
-                    <h4>$75.00</h4>
+                    <h3 class="text-xl mt-2 font-bold">Dream Sliding Mitt Smiley Melt</h3>
+                    <h4>$85.00</h4>
                 </div>
                 <div class="card">
-                    <div class="image bg-gray-100 p-5 rounded-xl overflow-hidden shadow md:h-[50vh] h-[30vh] flex items-center justify-center relative group cursor-pointer">
-                        <img src="/images/pro_4.png" alt="" class="group-hover:scale-125 transition duration-700 ease-in-out">
+                    <div class="image bg-gray-100 rounded-xl overflow-hidden shadow h-[48vh] flex items-center justify-center relative group cursor-pointer">
+                        <img src="/thumb/Dream-AS-Baby-Blue-Yellow.jpeg" alt="" class="group-hover:scale-125 transition duration-700 ease-in-out object-cover h-full w-full">
                         <button class="bg-white shadow-xl rounded-full h-10 w-10 items-center justify-center absolute top-4 right-4 text-gray-700 hidden group-hover:flex transition duration-300 ease-in-out">
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24"><path fill="currentColor" d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5M12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5s5 2.24 5 5s-2.24 5-5 5m0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3s3-1.34 3-3s-1.34-3-3-3"/></svg>
                         </button>
@@ -56,8 +56,8 @@
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 256 256"><path fill="currentColor" d="M96 216a16 16 0 1 1-16-16a16 16 0 0 1 16 16m88-16a16 16 0 1 0 16 16a16 16 0 0 0-16-16m47.65-125.65l-28.53 92.71A23.89 23.89 0 0 1 180.18 184H84.07A24.11 24.11 0 0 1 61 166.59L24.82 40H8a8 8 0 0 1 0-16h16.82a16.08 16.08 0 0 1 15.39 11.6L48.32 64H224a8 8 0 0 1 7.65 10.35M213.17 80H52.89l23.49 82.2a8 8 0 0 0 7.69 5.8h96.11a8 8 0 0 0 7.65-5.65Z"/></svg>
                         </button>
                     </div>
-                    <h3 class="text-xl mt-2 font-bold">Dream Belt</h3>
-                    <h4>$65.00 - $85.00</h4>
+                    <h3 class="text-xl mt-2 font-bold">Dream AS Baby Blue/ Yellow</h3>
+                    <h4>$25.00 – $35.00</h4>
                 </div>
             </div>
         </section>
@@ -68,14 +68,14 @@
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Baltimore_Orioles_batting_gloves_%287436167842%29.jpg/640px-Baltimore_Orioles_batting_gloves_%287436167842%29.jpg" alt="" class="w-full md:h-full h-[30vh] object-cover  group-hover:scale-125 transition duration-700 ease-in-out">
                 <div class="absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center bg-red-600/50">
                     <span class=" text-white font-bold md:text-[3em] text-[2.5em]">Batting <br/> Gloves</span>
-                    <RouterLink class="opacity-0 group-hover:opacity-100 bg-gray-900/75 backdrop-blur-md text-white px-5 py-2 rounded-lg" to="/customize">Customize now</RouterLink>
+                    <a class="opacity-0 group-hover:opacity-100 bg-gray-900/75 backdrop-blur-md text-white px-5 py-2 rounded-lg" href="{{route('customize-batting-gloves')}}">Customize now</a>
                 </div>
             </div>
             <div class="relative group overflow-hidden cursor-pointer">
                 <img src="https://preview.thenewsmarket.com/Previews/NBAS/StillAssets/960x540/640682.jpg" alt="" class="w-full md:h-full h-[30vh] object-cover  group-hover:scale-125 transition duration-700 ease-in-out">
                 <div class="absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center bg-indigo-600/50">
                     <span class=" text-white font-bold md:text-[3em] text-[2.5em]">Custom <br/> Gloves</span>
-                    <RouterLink class="opacity-0 group-hover:opacity-100 bg-gray-900/75 backdrop-blur-md text-white px-5 py-2 rounded-lg" to="/customize">Customize now</RouterLink>
+                    <a class="opacity-0 group-hover:opacity-100 bg-gray-900/75 backdrop-blur-md text-white px-5 py-2 rounded-lg" href="{{route('customize-gloves')}}">Customize now</a>
                 </div>
             </div>
             <div class="relative group overflow-hidden cursor-pointer">
@@ -94,34 +94,34 @@
             <div class="flex flex-col md:flex-row gap-5 md:h-screen">
                 <div class="md:w-[40%] w-full">
                     <figure class="rounded-2xl overflow-hidden w-full shadow-lg h-full relative group">
-                        <img src="/images/image_1.jpeg" class="md:h-full h-[40vh] w-full object-cover">
+                        <img src="/thumb/Black-Golden-Gloves.jpeg" class="md:h-full h-[40vh] w-full object-cover">
                         <div class="absolute inset-0 bg-black/75 flex items-center justify-center flex-col cursor-pointer transition-all duration-700 ease-in-out opacity-0 group-hover:opacity-100">
-                            <h3 class="text-white text-[3.5em] font-bold">Baseball</h3>
-                            <button class="bg-gray-50 text-sm rounded px-3 py-1">View all</button>
+                            <h3 class="text-white text-[3.5em] font-bold">Batting gloves</h3>
+                            <a href="{{route('baseball-batting-gloves-adult')}}" class="bg-gray-50 text-sm rounded px-3 py-1">View all</a>
                         </div>
                     </figure>
                 </div>
                 <div class="md:w-[60%] w-full flex flex-col gap-5">
                     <figure class="rounded-2xl overflow-hidden w-full shadow-lg h-[50%] relative group">
-                        <img src="/images/image_2.jpeg" class="h-full w-full object-cover">
+                        <img src="/thumb/Custom-Glove.jpeg" class="h-full w-full object-cover">
                         <div class="absolute inset-0 bg-black/75 flex items-center justify-center flex-col cursor-pointer transition-all duration-700 ease-in-out opacity-0 group-hover:opacity-100">
-                            <h3 class="text-white text-[3em] font-bold">Apparel</h3>
-                            <button class="bg-gray-50 text-sm rounded px-3 py-1">View all</button>
+                            <h3 class="text-white text-[3em] font-bold">Custom Gloves</h3>
+                            <a href="{{route('customize-gloves')}}" class="bg-gray-50 text-sm rounded px-3 py-1">View all</a>
                         </div>
                     </figure>
                     <div class="grid grid-cols-2 gap-5 h-[50%]">
                         <figure class="rounded-2xl overflow-hidden w-full shadow-lg h-full relative group">
-                            <img src="/images/image_3.jpeg" class="h-full w-full object-cover">
+                            <img src="/thumb/belt-red.png" class="h-full w-full object-cover">
                             <div class="absolute inset-0 bg-black/75 flex items-center justify-center flex-col cursor-pointer transition-all duration-700 ease-in-out opacity-0 group-hover:opacity-100">
-                                <h3 class="text-white text-[2em] font-bold">Clearance</h3>
-                                <button class="bg-gray-50 text-sm rounded px-3 py-1">View all</button>
+                                <h3 class="text-white text-[2em] font-bold">Belt</h3>
+                                <a href="{{route('apparel-belts')}}" class="bg-gray-50 text-sm rounded px-3 py-1">View all</a>
                             </div>
                         </figure>
                         <figure class="rounded-2xl overflow-hidden w-full shadow-lg h-full relative group">
-                            <img src="/images/image_4.jpeg" class="h-full w-full object-cover">
+                            <img src="/thumb/accessories.jpeg" class="h-full w-full object-cover">
                             <div class="absolute inset-0 bg-black/75 flex items-center justify-center flex-col cursor-pointer transition-all duration-700 ease-in-out opacity-0 group-hover:opacity-100">
                                 <h3 class="text-white text-[2em] font-bold">Accessories</h3>
-                                <button class="bg-gray-50 text-sm rounded px-3 py-1">View all</button>
+                                <a href="{{route('accessories')}}" class="bg-gray-50 text-sm rounded px-3 py-1">View all</a>
                             </div>
                         </figure>
                     </div>
