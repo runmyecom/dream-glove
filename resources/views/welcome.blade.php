475e70853ff72ff90d7736dd28bd2c07807e3429
Vimal Bharti <s.vimalbharti@gmail.com>
2024-06-05 15:55:12 +0530

    hero changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -67,21 +67,21 @@
         {{-- Categories grid --}}
         <div class="grid md:grid-cols-3">
             <div class="relative group overflow-hidden cursor-pointer">
-                <img src="/images/pro_1.png" alt="" class="w-full md:h-full h-[30vh] object-cover  group-hover:scale-125 transition duration-700 ease-in-out">
+                <img src="/images/pro_1.png" alt="" class="w-full h-[60vh] object-contain  group-hover:scale-125 transition duration-700 ease-in-out">
                 <div class="absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center bg-red-600/50">
                     <span class=" text-white font-bold md:text-[3em] text-[2.5em]">Batting <br/> Gloves</span>
                     <a class="opacity-0 group-hover:opacity-100 bg-gray-900/75 backdrop-blur-md text-white px-5 py-2 rounded-lg" href="{{route('customize-batting-gloves')}}">Customize now</a>
                 </div>
             </div>
             <div class="relative group overflow-hidden cursor-pointer">
-                <img src="/images/gloves.png" alt="" class="w-full md:h-full h-[30vh] object-cover  group-hover:scale-125 transition duration-700 ease-in-out">
+                <img src="/images/gloves.png" alt="" class="w-full h-[60vh] object-contain  group-hover:scale-125 transition duration-700 ease-in-out">
                 <div class="absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center bg-indigo-600/50">
                     <span class=" text-white font-bold md:text-[3em] text-[2.5em]">Custom <br/> Gloves</span>
                     <a class="opacity-0 group-hover:opacity-100 bg-gray-900/75 backdrop-blur-md text-white px-5 py-2 rounded-lg" href="{{route('customize-gloves')}}">Customize now</a>
                 </div>
             </div>
             <div class="relative group overflow-hidden cursor-pointer">
-                <img src="/images/pro_4.png" alt="" class="w-full md:h-full h-[30vh] object-cover  group-hover:scale-125 transition duration-700 ease-in-out">
+                <img src="/images/pro_4.png" alt="" class="w-full h-[60vh] object-contain  group-hover:scale-125 transition duration-700 ease-in-out">
                 <div class="absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center bg-yellow-600/50">
                     <span class=" text-white font-bold md:text-[3em] text-[2.5em]">Custom <br/> Belts</span>
                     <RouterLink class="opacity-0 group-hover:opacity-100 bg-gray-900/75 backdrop-blur-md text-white px-5 py-2 rounded-lg" to="/customize">Customize now</RouterLink>
