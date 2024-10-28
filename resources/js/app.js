3e6fd9a1bff74de68b117a1c41565bfb3baf6e7d
Vimal Bharti <s.vimalbharti@gmail.com>
2024-10-28 12:21:54 +0530

    new

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,61 +3,8 @@ import './bootstrap';
 import "toastify-js/src/toastify.css";
 import Toastify from 'toastify-js';
 
-import { gsap } from "gsap";
-
-import { Flip } from "gsap/Flip";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { ScrollToPlugin } from "gsap/ScrollToPlugin";
-
-gsap.registerPlugin(Flip,ScrollTrigger,ScrollToPlugin);
-
 window.Toastify = Toastify;
 
 
 
-gsap.from('.heroGlove', {
-    opacity:0,
-    scale:.5,
-    duration: 1,
-    delay:.5
-})
 
-gsap.utils.toArray('.title').forEach(title=>{
-    gsap.fromTo(title,{
-        opacity:0,
-        x:-300,
-        skewX:65
-    },{
-        opacity:1,
-        x:0,
-        skewX:0,
-        delay:.5,
-        duration:1,
-        scrollTrigger:title
-    })
-})
-
-gsap.utils.toArray('button').forEach(button=>{
-    gsap.fromTo(button,{
-        opacity:0,
-    },{
-        opacity:1,
-        duration:1,
-        delay:1,
-        scrollTrigger:button
-    })
-})
-
-gsap.fromTo('.card' ,{
-    opacity:0,
-    scale:.1,
-},{
-    opacity:1,
-    scale:1,
-    duration:1,
-    delay:.5,
-    stagger:{
-        amount:1
-    },
-    scrollTrigger:'.card'
-})
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -174,6 +174,55 @@
         <x-footer />
     </div>
 
+    <script>
+        gsap.from('.heroGlove', {
+    opacity:0,
+    scale:.5,
+    duration: 1,
+    delay:.5
+})
+
+gsap.utils.toArray('.title').forEach(title=>{
+    gsap.fromTo(title,{
+        opacity:0,
+        x:-300,
+        skewX:65
+    },{
+        opacity:1,
+        x:0,
+        skewX:0,
+        delay:.5,
+        duration:1,
+        scrollTrigger:title
+    })
+})
+
+gsap.utils.toArray('button').forEach(button=>{
+    gsap.fromTo(button,{
+        opacity:0,
+    },{
+        opacity:1,
+        duration:1,
+        delay:1,
+        scrollTrigger:button
+    })
+})
+
+gsap.fromTo('.card' ,{
+    opacity:0,
+    scale:.1,
+},{
+    opacity:1,
+    scale:1,
+    duration:1,
+    delay:.5,
+    stagger:{
+        amount:1
+    },
+    scrollTrigger:'.card'
+})
+    </script>
+
     <style>
         .heroGlove{
             animation: float 3s ease-in-out infinite;
